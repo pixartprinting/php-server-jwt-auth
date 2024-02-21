b8fd8b964157bcb3ddbf365d0b0efa1572bcf592
ayushjain-cimpress <ayush.jain@pixartprinting.com>
2024-02-21 15:20:16 +0530

    boilerplate publish

---
+++ README.md
@@ -0,0 +1,25 @@
+# PHP Server JWT Auth
+
+PHP library for supporting Cimpress and Auth0 JWT on server side
+
+## Dependencies
+
+* PHP >= 8.1
+* ext-json: *,
+* firebase/php-jwt: ^6.8
+
+## Installation via Composer
+
+```json
+{
+  "repositories": [
+    {
+      "url": "https://github.com/pixartprinting/php-server-jwt-auth.git",
+      "type": "git"
+    }
+  ],
+  "require": {
+    "pixartprinting/php-server-jwt-auth": "master"
+  }
+}
+```
---
+++ tests/Unit/JWTTest.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Tests;
+
+trait JWTTest
+{
+    public function testValidAuth0Token()
+    {
+
+    }
+
+    public function testValidCimpressToken()
+    {
+
+    }
+
+    public function testExpiredAuth0Token()
+    {
+
+    }
+
+    public function testExpiredCimpressToken()
+    {
+
+    }
+
+    public function testUnsupportedIssuer()
+    {
+
+    }
+
+    public function testTemperedToken()
+    {
+
+    }
+
+    public function testMissingJWKSFile()
+    {
+
+    }
+}
